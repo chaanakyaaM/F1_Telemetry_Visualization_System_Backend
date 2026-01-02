84eb8eb8413b54bcb5e071f6118b9a25f53b12d8
unknown <chaanakyaam@gmail.com>
2026-01-02 23:58:36 +0530

    cache error fixed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 
 
-fastf1.Cache.enable_cache("cache")
+fastf1.Cache.enable_cache("cache_f1")
 
 load_dotenv()
 
